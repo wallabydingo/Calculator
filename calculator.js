9bba8e41fc16c29d167ae8a8f4747f564345e667
Wallaby Dingo <wallabydingo@me.com>
2022-02-06 22:34:45 +1000

    added operate function

--- calculator.js
+++ calculator.js
@@ -1,15 +1,39 @@
 const add = function(a, b) {
-    return a + b;
+    // return a + b;
+    console.log(a + b);
   };
   
 const subtract = function(a, b) {
-    return a - b;
+    // return a - b;
+    console.log(a - b);
   };
   
 const multiply = function(a, b) {
-      return a * b;
+      //return a * b;
+      console.log(a * b);
   };
   
 const divide = function(a, b) {
-    return a / b;
+    //return a / b;
+    if (b === 0) {
+        console.log("Can't divide by zero, ya clown!");
+    } else {console.log(a / b);}
+    
   };
+
+const operate = function(operator, a, b) {
+    switch(operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+    };
+};
