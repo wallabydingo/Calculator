1b09d7f627d2945f1ca6325ec0762993f77afb19
Wallaby Dingo <wallabydingo@me.com>
2022-02-10 23:01:06 +1000

    trying to get calculator logic (FUUUUCCCCK)

--- calculator.js
+++ calculator.js
@@ -1,25 +1,24 @@
 const add = function(a, b) {
-    // return a + b;
-    console.log(a + b);
+    //return a + b;
+    console.log(a+b);
   };
   
 const subtract = function(a, b) {
-    // return a - b;
-    console.log(a - b);
+    return a - b;
+    
   };
   
 const multiply = function(a, b) {
-      //return a * b;
-      console.log(a * b);
+      return a * b;
   };
   
 const divide = function(a, b) {
-    //return a / b;
     if (b === 0) {
         console.log("Can't divide by zero, ya clown!");
-    } else {console.log(a / b);}
+    } else {return a / b;}
     
   };
+
 
 const operate = function(operator, a, b) {
     switch(operator) {
@@ -44,10 +43,48 @@ const clearScreen = function() {
     screen = '';
     document.getElementById("display").innerHTML = 0;
 };
-
-const screenAdd = function(a) {
-    screen = screen.concat(a);
+let firstNo = '';
+    let oper = '';
+const screenAdd = function(keypress) {
+    
+    screen = screen.concat(keypress);
     document.getElementById("display").innerHTML = screen;
-    console.log(screen);
+    
+    if (isNaN(keypress) == false) {
+        firstNo = firstNo.concat(keypress);
+        console.log("firstno " +firstNo);
+    } else {
+        oper = oper.concat(keypress);
+        firstLen = firstNo.length;
+        console.log("operator "+oper);
+        if (oper.length > 1) {
+            
+        
+            secondNo = screen.replace(firstNo.slice(0,firstLen-2)+oper.slice(0,1),'');
+            show = operate(oper.slice(0,1),parseFloat(firstNo.replace(secondNo.slice(0,-1),'')),parseFloat(secondNo.slice(0,-1)));
+            console.log(operate("+",12,34));
+            screen = operate(oper, firstNo, secondNo.slice(0,-1)) + oper.slice(-1);
+            console.log("screen "+screen);
+            oper = oper.slice(-1);
+            firstNo = operate(oper, firstNo, secondNo.slice(0,-1));
+        };
+    };
+    
+    
+        
+    
+    /* if (isNaN(screen) == true) {
+        oper = screen.slice(-1);
+        firstNo = screen.slice(0,-1);
+        console.log(oper);
+        console.log(firstNo);
+    };
+     */
+};
+
+const evaluate = function(display) {
+    for (let i = 0; i < display.length; i++) {
+        process(display[i]);
+      }
 };
 
