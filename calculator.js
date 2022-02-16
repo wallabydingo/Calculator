a05f4a8734a5d826ad6e782949bac99223bbbd1b
Wallaby Dingo <wallabydingo@me.com>
2022-02-16 22:20:48 +1000

    It works!

--- calculator.js
+++ calculator.js
@@ -58,54 +58,26 @@ const screenAdd = function(keypress) {
     screen = screen.concat(keypress);
     document.getElementById("display").innerHTML = screen;
     
-    if (isNaN(keypress) == false) {
-        firstNo = firstNo.concat(keypress);
-        console.log("firstno " +firstNo);
-    } else {
+    if (isNaN(keypress) == true) {
         oper = oper.concat(keypress);
-        firstLen = firstNo.length;
-        console.log("firstno digits: "+firstLen);
-        console.log("operator "+oper);
         if (oper.length > 1) {
-            
-            console.log(firstNo);
-            secondNo = screen.replace(firstNo.slice(0,firstLen-2)+oper.slice(0,1),'');
-            console.log(secondNo);
-            //show = operate(oper.slice(0,1),parseFloat(firstNo.replace(secondNo.slice(0,-1),'')),parseFloat(secondNo.slice(0,-1)));
-            console.log(oper.slice(0,1));
-            //console.log(parseFloat(firstNo.replace(secondNo.slice(0,-1),'')));
-            firstNo = parseFloat(firstNo.replace(secondNo.slice(0,-1),''));
-            console.log(firstNo);
-            secondNo = parseFloat(secondNo.slice(0,-1));
-            console.log(secondNo);
-            firstNo = operate(oper.slice(0,1),firstNo,secondNo).toString();
-            oper = oper.slice(-1);
-            screen = firstNo + oper;
-            console.log(firstNo);
-            console.log(oper);
-            document.getElementById("display").innerHTML = screen;
-            //screen = operate("+",11,22);
-            //console.log(screen);
-            //oper = oper.slice(-1);
-            //firstNo = operate(oper, firstNo, secondNo);
+            if (oper.slice(-1) == "=") {
+                firstNo = parseFloat(screen.split(oper.slice(0,1))[0]);
+                secondNo = screen.replace(firstNo+oper.slice(0,1),'');
+                secondNo = parseFloat(secondNo.replace(oper.slice(-1),''));
+                firstNo = operate(oper.slice(0,1),firstNo,secondNo).toString();
+                document.getElementById("display").innerHTML = firstNo;
+            } else {
+                firstNo = parseFloat(screen.split(oper.slice(0,1))[0]);
+                secondNo = screen.replace(firstNo+oper.slice(0,1),'');
+                secondNo = parseFloat(secondNo.replace(oper.slice(-1),''));
+                firstNo = operate(oper.slice(0,1),firstNo,secondNo).toString();
+                oper = oper.slice(-1);
+                screen = firstNo + oper;
+                document.getElementById("display").innerHTML = screen;
+            };
         };
-    };
-    
-    
         
-    
-    /* if (isNaN(screen) == true) {
-        oper = screen.slice(-1);
-        firstNo = screen.slice(0,-1);
-        console.log(oper);
-        console.log(firstNo);
     };
-     */
+     
 };
-
-const evaluate = function(display) {
-    for (let i = 0; i < display.length; i++) {
-        process(display[i]);
-      }
-};
-
